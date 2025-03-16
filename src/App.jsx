612bbc9d833ca925f7e5f9354ad06eb4f52369bf
Parthipan Kavin <kavin@Kavins-MacBook-Pro.local>
2025-03-16 17:03:50 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Profile from './components/Profile.jsx'
+import About from './components/About.jsx'
+import Service from './components/Service.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Certifications from './components/Certifications.jsx'
+import TechSkills from './components/Techskills.jsx'
+import Experience from './components/Experience.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Profile />
+      <About />
+      <Service />
+      <Projects />
+      <Experience />
+      <Certifications />
+      <TechSkills />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Certifications.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Certifications = () => {
+    return (
+        <div className='bg-black text-white py-20' id='certifications'>
+            <div className='container mx-auto px-8 md:px-16 lg:px-24'>
+                <h2 className='text-4xl font-bold text-center mb-12'>Certifications</h2>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Certifications
