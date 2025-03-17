2d67ebfa120892afbf3968e20ebf2fceaaec2a92
Parthipan Kavin <kavin@Kavins-MacBook-Pro.local>
2025-03-17 08:05:57 +0800

    prep for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg']
+  assetsInclude: ['**/*.JPG', '**/*.jpg'],
+  base: "/Portfolio_Website/"
 })
 
 
